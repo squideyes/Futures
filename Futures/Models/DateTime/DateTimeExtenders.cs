28d2183f9ce66f33b3417d2e449a0900f52fd903
Louis S. Berman <louis@squideyes.com>
2022-12-20 11:34:55 -0500

    Added IntervalExtenders

--- Futures/Models/DateTime/DateTimeExtenders.cs
+++ Futures/Models/DateTime/DateTimeExtenders.cs
@@ -1,4 +1,9 @@
-﻿namespace SquidEyes.Futures;
+﻿// ********************************************************
+// The use of this source code is licensed under the terms
+// of the MIT License (https://opensource.org/licenses/MIT)
+// ********************************************************
+
+namespace SquidEyes.Futures;
 
 public static class DateTimeExtenders
 {
